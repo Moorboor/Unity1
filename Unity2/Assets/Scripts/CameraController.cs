180f7b88de3f401a646f56b2421d744cce18c722
Moorboor <moorboory@gmail.com>
2022-05-23 16:14:57 +0200

    Tested visual studio mac

--- Unity2/Assets/Scripts/CameraController.cs
+++ Unity2/Assets/Scripts/CameraController.cs
@@ -18,15 +18,13 @@ public class CameraController : MonoBehaviour
     private float currentZoom = 10f;
     private float currentYaw = 0f;
 
-    void Update()
-    {
+    void Update() {
         currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed; 
         currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
 
         currentYaw -= Input.GetAxis("Horizontal") * yawSpeed * Time.deltaTime;
     }
-    void LateUpdate()
-    {
+    void LateUpdate() {
         transform.position = target.position - offset * currentZoom;
         transform.LookAt(target.position + Vector3.up * pitch);
         transform.RotateAround(target.position, Vector3.up, currentYaw);
